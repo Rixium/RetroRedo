4c293f71efeb064e8feba00d09194876bbd4377f
Daniel Pottenger <gamerpotty@gmail.com>
2020-10-03 14:16:28 +0100

    Add map rendering to the game screen

--- Screen/GameScreen.cs
+++ Screen/GameScreen.cs
@@ -1,8 +1,9 @@
 ﻿using System;
+using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using RetroRedo.Content;
-using RetroRedo.Exceptions;
+using RetroRedo.Data;
 using RetroRedo.Maps;
 
 namespace RetroRedo.Screen
@@ -10,29 +11,30 @@ namespace RetroRedo.Screen
     public class GameScreen : IScreen
     {
         private readonly IContentChest _contentChest;
+        private readonly IGameStateService _gameStateService;
         private readonly IMapLoader _mapLoader;
-        public ScreenType ScreenType => ScreenType.Game;
+        private readonly IMapRenderer _mapRenderer;
 
+        private TiledMap _activeTiledMap;
+
+        public ScreenType ScreenType => ScreenType.Game;
         public bool Ended { get; private set; }
         public Action<ScreenType> RequestScreenChange { get; set; }
 
-        public GameScreen(IContentChest contentChest, IMapLoader mapLoader)
+        public GameScreen(IContentChest contentChest, IGameStateService gameStateService, IMapLoader mapLoader,
+            IMapRenderer mapRenderer)
         {
             _contentChest = contentChest;
+            _gameStateService = gameStateService;
             _mapLoader = mapLoader;
-
-            try
-            {
-                var currentMap = _mapLoader.LoadMap(1);
-            }
-            catch (MapNotExistException mapNotExistException)
-            {
-                Console.WriteLine(mapNotExistException.Message);
-            }
+            _mapRenderer = mapRenderer;
         }
-        
+
         public void Begin()
         {
+            var activeMapId = _gameStateService.CurrentLevel;
+            _activeTiledMap = _mapLoader.LoadMap(activeMapId);
+            _mapRenderer.SetMap(_activeTiledMap);
         }
 
         public void Update()
@@ -41,9 +43,20 @@ namespace RetroRedo.Screen
 
         public void Render(SpriteBatch spriteBatch)
         {
-            spriteBatch.Begin();
-            spriteBatch.Draw(_contentChest.Get<Texture2D>("Images/pixel"), new Rectangle(0, 0, 1280, 720), new Color(9, 22, 48));
+            spriteBatch.Begin(SpriteSortMode.Deferred, null, SamplerState.PointClamp);
+
+            spriteBatch.Draw(_contentChest.Get<Texture2D>("Images/pixel"), new Rectangle(0, 0, 1280, 720),
+                new Color(9, 22, 48));
+
+            spriteBatch.DrawString(_contentChest.Get<SpriteFont>("Fonts/MainFont"),
+                _activeTiledMap.Properties.First(x => x.Name.Equals("Name")).Value,
+                new Vector2(40, 40), Color.White);
+
+            _mapRenderer.Render(spriteBatch);
+
             spriteBatch.End();
         }
+
+        public bool Stop { get; set; }
     }
 }
