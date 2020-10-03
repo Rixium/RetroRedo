8c10bf8f5e42ed5587421ebaa8e5fb17095c1cbc
Daniel Pottenger <gamerpotty@gmail.com>
2020-10-03 14:49:29 +0100

    Add map type

---
+++ Maps/Map.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using RetroRedo.Entities;
+
+namespace RetroRedo.Maps
+{
+    public class Map
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int MapWidth { get; set; }
+        public int MapHeight { get; set; }
+        public Tile[,] Tiles { get; set; }
+
+        private IList<IEntity> _mapEntities = new List<IEntity>();
+    }
+}
