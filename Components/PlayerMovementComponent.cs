1adb52642aea9d3672a2fcbe07fa60e784e2b5a4
Daniel Pottenger <gamerpotty@gmail.com>
2020-10-03 15:39:27 +0100

    Add player move component

---
+++ Components/PlayerMovementComponent.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework.Input;
+using RetroRedo.Commands;
+using RetroRedo.Entities;
+using RetroRedo.Input;
+
+namespace RetroRedo.Components
+{
+    public class PlayerMovementComponent : IComponent
+    {
+        private readonly IInputService _inputService;
+
+        public IEntity Entity { get; set; }
+
+        public PlayerMovementComponent(IInputService inputService)
+        {
+            _inputService = inputService;
+
+            _inputService.OnKeyPressed(Keys.D, () =>
+            {
+                var commandSetComponent = Entity.GetComponent<CommandSetComponent>();
+                commandSetComponent.PushCommand(new MoveCommand(1, 0));
+            });
+            
+            _inputService.OnKeyPressed(Keys.A, () =>
+            {
+                var commandSetComponent = Entity.GetComponent<CommandSetComponent>();
+                commandSetComponent.PushCommand(new MoveCommand(-1, 0));
+            });
+            
+            _inputService.OnKeyPressed(Keys.S, () =>
+            {
+                var commandSetComponent = Entity.GetComponent<CommandSetComponent>();
+                commandSetComponent.PushCommand(new MoveCommand(0, 1));
+            });
+            
+            _inputService.OnKeyPressed(Keys.W, () =>
+            {
+                var commandSetComponent = Entity.GetComponent<CommandSetComponent>();
+                commandSetComponent.PushCommand(new MoveCommand(0, -1));
+            });
+            
+            _inputService.OnKeyPressed(Keys.Z, () =>
+            {
+                var commandSetComponent = Entity.GetComponent<CommandSetComponent>();
+                commandSetComponent.Undo();
+            });
+        }
+
+        public void Update()
+        {
+        }
+    }
+}
