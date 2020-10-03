138ffd3a020154d3715e2fdae6b3818136594fb8
Daniel Pottenger <gamerpotty@gmail.com>
2020-10-03 15:37:45 +0100

    Add move command

---
+++ Commands/ICommand.cs
@@ -0,0 +1,10 @@
+﻿using RetroRedo.Entities;
+
+namespace RetroRedo.Commands
+{
+    public interface ICommand
+    {
+        void Do(IEntity entity);
+        void Undo(IEntity entity);
+    }
+}
---
+++ Commands/MoveCommand.cs
@@ -0,0 +1,28 @@
+﻿using RetroRedo.Entities;
+
+namespace RetroRedo.Commands
+{
+    public class MoveCommand : ICommand
+    {
+        private readonly int _xChange;
+        private readonly int _yChange;
+
+        public MoveCommand(int xChange, int yChange)
+        {
+            _xChange = xChange;
+            _yChange = yChange;
+        }
+
+        public void Do(IEntity entity)
+        {
+            entity.X += _xChange;
+            entity.Y += _yChange;
+        }
+
+        public void Undo(IEntity entity)
+        {
+            entity.X -= _xChange;
+            entity.Y -= _yChange;
+        }
+    }
+}
