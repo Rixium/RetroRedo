7639d0d480fcef477380d4d751fe2595b2b7c25f
Daniel Pottenger <gamerpotty@gmail.com>
2020-10-04 22:47:49 +0100

    Use complimentary colours for blocks

--- Entities/Player.cs
+++ Entities/Player.cs
@@ -1,47 +1,18 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using RetroRedo.Content;
-using RetroRedo.Maps;
-using RetroRedo.Screen;
 
 namespace RetroRedo.Entities
 {
     public class Player : Entity
     {
-        
-        private float _colorTimer;
-        private Color _currentColor;
-        private float _current;
-
         public Player(int x, int y)
         {
             X = x;
             Y = y;
-            
-            var rainbow = MapRenderer.Rainbow(_current);
-            _currentColor = new Color(rainbow.R, rainbow.G, rainbow.B);
+
         }
-
-        public override void AnyTimeUpdate()
-        {
-            _colorTimer += GameTimeService.DeltaTime;
-
-            if (_colorTimer >= 0.03f)
-            {
-                _current += 0.01f;
-                if (_current > 1)
-                {
-                    _current = 0;
-                }
-                
-                var rainbow = MapRenderer.Rainbow(_current);
-                _currentColor = new Color(rainbow.R, rainbow.G, rainbow.B);
-                _colorTimer = 0;
-            }
-
-            base.AnyTimeUpdate();
-        }
-
+        
         public override void Entered(IEntity other)
         {
             // Does nothing.
@@ -54,8 +25,7 @@ namespace RetroRedo.Entities
         public override void Render(SpriteBatch spriteBatch)
         {
             spriteBatch.Draw(ContentChest.Get<Texture2D>("Images/player"),
-                new Vector2(Tile.RenderX, Tile.RenderY),
-                _currentColor);
+                new Vector2(Tile.RenderX, Tile.RenderY), new Color(182, 158, 121));
         }
     }
 }
--- Maps/MapRenderer.cs
+++ Maps/MapRenderer.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using RetroRedo.Content;
 using RetroRedo.Entities;
-using RetroRedo.Window;
 
 namespace RetroRedo.Maps
 {
@@ -13,7 +11,6 @@ namespace RetroRedo.Maps
         private const int TileRenderSize = 16;
         private const int ActualTileSize = 16;
         
-        Random random = new Random();
         HashSet<Tile> _historyTiles = new HashSet<Tile>();
 
         private Map _map;
@@ -28,9 +25,6 @@ namespace RetroRedo.Maps
             }
 
             var tileSet = ContentChest.Get<Texture2D>("Images/tiles_1");
-            var screenCenter = WindowSettings.Center;
-            var mapWidthInPixels = _map.MapWidth * TileRenderSize;
-            var mapHeightInPixels = _map.MapHeight * TileRenderSize;
 
             foreach (var tile in _map.Tiles)
             {
@@ -60,26 +54,22 @@ namespace RetroRedo.Maps
                 {
                     entitiesOnPlayer++;
                     
-                    var rainbow = Rainbow(entitiesOnPlayer / 10.0f);
-
                     spriteBatch.Draw(ContentChest.Get<Texture2D>("Images/player"),
                         new Vector2(
                             entity.X * TileRenderSize,
                             entity.Y * TileRenderSize - 3 * entitiesOnPlayer),
-                        new Color(rainbow.R, rainbow.G, rainbow.B) * 0.3f);
+                        new Color(176, 182, 121) * (1.0f / entitiesOnPlayer));
                 }
                 else
                 {
                     var entityTile = _map.Tiles[entity.X, entity.Y];
                     var entitiesHere = entityTile.ThisFrameHistoryCount;
-
-                    var rainbow = Rainbow(entitiesHere / 10.0f);
                     
                     spriteBatch.Draw(ContentChest.Get<Texture2D>("Images/player"),
                         new Vector2(
                             entity.X * TileRenderSize,
                             entity.Y * TileRenderSize - 3 * entitiesHere),
-                        new Color(rainbow.R, rainbow.G, rainbow.B) * 0.3f);
+                        new Color(176, 182, 121) * (1.0f / (entitiesHere + 1)));
 
                     entityTile.ThisFrameHistoryCount++;
 
@@ -95,27 +85,5 @@ namespace RetroRedo.Maps
             _historyTiles.Clear();
         }
         
-        public static System.Drawing.Color Rainbow(float progress)
-        {
-            var div = (Math.Abs(progress % 1) * 6);
-            var ascending = (int) ((div % 1) * 255);
-            var descending = 255 - ascending;
-
-            switch ((int) div)
-            {
-                case 0:
-                    return System.Drawing.Color.FromArgb(255, 255, ascending, 0);
-                case 1:
-                    return System.Drawing.Color.FromArgb(255, descending, 255, 0);
-                case 2:
-                    return System.Drawing.Color.FromArgb(255, 0, 255, ascending);
-                case 3:
-                    return System.Drawing.Color.FromArgb(255, 0, descending, 255);
-                case 4:
-                    return System.Drawing.Color.FromArgb(255, ascending, 0, 255);
-                default: // case 5:
-                    return System.Drawing.Color.FromArgb(255, 255, 0, descending);
-            }
-        }
     }
 }
